3ffaf47e740953eaa5c312c9f61a1c52230740c4
rcyyu <ronald.c.yu@gmail.com>
2017-10-16 18:58:56 +0000

    Initial Commit

---
+++ leaflet-volume-map/scripts.js
@@ -0,0 +1,42 @@
+// Setting map properties
+var map = L.map("map", { zoomControl:false });
+
+//var bounds = new L.LatLngBounds(new L.LatLng(43.6473, -79.4044), new L.LatLng(43.64702, -79.3702));
+
+var streetLayer = Tangram.leafletLayer({
+    scene: 'scene.yaml',
+    events: {
+        click: onMapClick,
+    },
+    selectionRadius: 25
+});
+
+//map.setMaxBounds(bounds);
+
+// Disable dragging when user's cursor enters the element
+map.dragging.disable();
+map.touchZoom.disable();
+map.doubleClickZoom.disable();
+map.scrollWheelZoom.disable();
+map.boxZoom.disable();
+map.keyboard.disable();
+
+
+streetLayer.addTo(map);
+
+map.setView([43.6493, -79.3910], 15);
+
+// select road feature
+function onMapClick(selection) {
+    if (selection.feature) {
+        highlightSelect(selection.feature.properties.__roads_properties__.geo_id);
+    } else {
+        highlightSelect(false);
+    }
+}
+
+// highlight selection
+function highlightSelect(symbol) {
+    streetLayer.scene.config.global._highlightSelect = symbol;
+    streetLayer.scene.updateConfig();
+}
