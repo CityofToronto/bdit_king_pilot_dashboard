c922652783d80356dbead22155ad371497340197
rcyyu <ronald.c.yu@gmail.com>
2017-10-18 07:54:05 -0400

    Connected datasource to street_volumes.json

--- leaflet-volume-map/scripts.js
+++ leaflet-volume-map/scripts.js
@@ -3,6 +3,16 @@ var map = L.map("map", { zoomControl:false });
 
 //var bounds = new L.LatLngBounds(new L.LatLng(43.6473, -79.4044), new L.LatLng(43.64702, -79.3702));
 
+//var street_volumes;
+
+//$.getJSON("/street_volumes.geojson", function(json) {
+//    L.geoJSON(json).addTo(map); // this will show the info it in firebug console
+//    //console.log(street_volumes);
+//    console.log(json);
+//});
+
+//L.geoJSON(street_volumes).addTo(map);
+
 var streetLayer = Tangram.leafletLayer({
     scene: 'scene.yaml',
     events: {
@@ -14,12 +24,12 @@ var streetLayer = Tangram.leafletLayer({
 //map.setMaxBounds(bounds);
 
 // Disable dragging when user's cursor enters the element
-map.dragging.disable();
-map.touchZoom.disable();
-map.doubleClickZoom.disable();
-map.scrollWheelZoom.disable();
-map.boxZoom.disable();
-map.keyboard.disable();
+//map.dragging.disable();
+//map.touchZoom.disable();
+//map.doubleClickZoom.disable();
+//map.scrollWheelZoom.disable();
+//map.boxZoom.disable();
+//map.keyboard.disable();
 
 
 streetLayer.addTo(map);
