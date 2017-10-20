9b508dfed7418c962db515685952e66ac79e3f11
rcyyu <ronald.c.yu@gmail.com>
2017-10-20 15:39:02 -0400

    Updated style

--- leaflet-volume_map/scripts.js
+++ leaflet-volume_map/scripts.js
@@ -10,42 +10,45 @@ var streetLayer = Tangram.leafletLayer({
     selectionRadius: 25
 });
 
-var legend = L.control({position: 'topleft'});
-
-legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend');
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    div.innerHTML +=
-        '<span style="background:#FB998E"></span><label>↑20%</label>' +
-        '<span style="background:#FBA3BB"></span><label>↑20%&ndash;15%</label>' +
-        '<span style="background:#E2B9E1"></span><label>↑15%&ndash;10%</label>' +
-        '<span style="background:#B7D1F4"></span><label>↑10%&ndash;0%</label>' +
-        '<span style="background:#8FE7EE"></span><label>↓0%&ndash;10%</label>' +
-        '<span style="background:#8EF6D2"></span><label>↓10%&ndash;15%</label>' +
-        '<span style="background:#B7FEAC"></span><label>↓15%&ndash;20%</label>' +
-        '<span style="background:#F2FE8E"></span><label>↓20%</label>';
-    return div;
-};
-
-legend.addTo(map);
+// replaced by HTML hardcode
+//var legend = L.control({position: 'topleft'});
+//
+//legend.onAdd = function (map) {
+//
+//    var div = L.DomUtil.create('div', 'info legend');
+//
+//    // loop through our density intervals and generate a label with a colored square for each interval
+//    div.innerHTML +=
+//        '<span style="background:#FB998E"></span><label>↑20%</label>' +
+//        '<span style="background:#FBA3BB"></span><label>↑20%&ndash;15%</label>' +
+//        '<span style="background:#E2B9E1"></span><label>↑15%&ndash;10%</label>' +
+//        '<span style="background:#B7D1F4"></span><label>↑10%&ndash;0%</label>' +
+//        '<span style="background:#8FE7EE"></span><label>↓0%&ndash;10%</label>' +
+//        '<span style="background:#8EF6D2"></span><label>↓10%&ndash;15%</label>' +
+//        '<span style="background:#B7FEAC"></span><label>↓15%&ndash;20%</label>' +
+//        '<span style="background:#F2FE8E"></span><label>↓20%</label>';
+//    return div;
+//};
+//
+//legend.addTo(map);
 
 var myIcon = L.divIcon({className: 'label'});
 
+var vertLabel = [{coord:[43.65263, -79.40645], name:"Queen", dir:"left"},
+                 {coord:[43.65195, -79.40645], name:"Richmond", dir:"left"},
+                 {coord:[43.65053, -79.40645], name:"Adelaide", dir:"left"},
+                 {coord:[43.64924, -79.40645], name:"King", dir:"left"},
+                 {coord:[43.64777, -79.40645], name:"Wellington", dir:"left"},
+                 {coord:[43.64596, -79.40645], name:"Front", dir:"left"},
+                 {coord:[43.65325, -79.40602], name:"Bathurst", dir:"center"},
+                 {coord:[43.65325, -79.39808], name:"Spadina", dir:"center"},
+                 {coord:[43.65325, -79.38791], name:"University", dir:"center"},
+                 {coord:[43.65325, -79.38022], name:"Yonge", dir:"center"},
+                 {coord:[43.65325, -79.37394], name:"Jarvis", dir:"center"}];
 
-L.marker([43.65263, -79.40645], {icon: myIcon}).bindTooltip("Queen", {direction: "center", permanent: true, direction: "left", className: 'label'}).addTo(map);
-L.marker([43.65195, -79.40645], {icon: myIcon}).bindTooltip("Richmond", {direction: "center", permanent: true, direction: "left", className: 'label'}).addTo(map);
-L.marker([43.65053, -79.40645], {icon: myIcon}).bindTooltip("Adelaide", {direction: "center", permanent: true, direction: "left", className: 'label'}).addTo(map);
-L.marker([43.64924, -79.40645], {icon: myIcon}).bindTooltip("King", {direction: "center", permanent: true, direction: "left", className: 'label'}).addTo(map);
-L.marker([43.64777, -79.40645], {icon: myIcon}).bindTooltip("Wellington", {direction: "center", permanent: true, direction: "left", className: 'label'}).addTo(map);
-L.marker([43.64596, -79.40645], {icon: myIcon}).bindTooltip("Front", {direction: "center", permanent: true, direction: "left", className: 'label'}).addTo(map);
-
-L.marker([43.65300, -79.40602], {icon: myIcon}).bindTooltip("Bathurst", {direction: "center", permanent: true, className: 'label'}).addTo(map);
-L.marker([43.65300, -79.39808], {icon: myIcon}).bindTooltip("Spadina", {direction: "center", permanent: true, className: 'label'}).addTo(map);
-L.marker([43.65300, -79.38791], {icon: myIcon}).bindTooltip("University", {direction: "center", permanent: true, className: 'label'}).addTo(map);
-L.marker([43.65300, -79.38022], {icon: myIcon}).bindTooltip("Yonge", {direction: "center", permanent: true, className: 'label'}).addTo(map);
-L.marker([43.65300, -79.37394], {icon: myIcon}).bindTooltip("Jarvis", {direction: "center", permanent: true, className: 'label'}).addTo(map);
+for(var i = 0; i < vertLabel.length; i++) {
+    L.marker(vertLabel[i].coord, {icon: myIcon}).bindTooltip(vertLabel[i].name, {direction: vertLabel[i].dir, permanent: true, className: 'label'}).addTo(map);
+}
 
 // Disable dragging when user's cursor enters the element
 //map.dragging.disable();
