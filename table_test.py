d1c63285a4455a2affb8b45306049735fab50d6c
Aakash Harpalani <aharpal@toronto.ca>
2017-11-06 18:22:39 -0500

    Format changes to data table

--- table_test.py
+++ table_test.py
@@ -9,7 +9,20 @@ import pandas as pd
 
 df = pd.read_csv("data/test.csv")
 
-def generate_table(dataframe, max_rows=30):
+def generate_cell_class(colNum):
+    if colNum == 0:
+        return 'segname'
+    else:
+        return 'segother'
+    
+def generate_row_class(selected_str, current_str):
+    # if selected_str == current_str:
+    if current_str == 'Queen':
+        return 'selected';
+    else:
+        return 'notselected';
+
+def generate_table(dataframe, street_name=None, max_rows=30):
     return html.Table(
         [
          html.Tr( [html.Td(""), html.Td("Eastbound",colSpan=2), html.Td("Westbound",colSpan=2)] )
@@ -18,8 +31,8 @@ def generate_table(dataframe, max_rows=30):
          html.Tr( [html.Td(""), html.Td("After"), html.Td("Baseline"), html.Td("After"), html.Td("Baseline")] )
         ] +
         [html.Tr([
-            html.Td(dataframe.iloc[i][col], id = dataframe.iloc[i]['street'] + col) for col in dataframe.columns
-        ], id= dataframe.iloc[i]['street'], n_clicks=0) for i in range(min(len(dataframe), max_rows))]
+            html.Td(dataframe.iloc[i][col], id = (dataframe.iloc[i]['street'] + '_' + str(dataframe.columns.get_loc(col)+1)), className = generate_cell_class(dataframe.columns.get_loc(col))) for col in dataframe.columns
+        ], id= dataframe.iloc[i]['street'], className = generate_row_class(street_name, dataframe.iloc[i]['street']), n_clicks=0) for i in range(min(len(dataframe), max_rows))]
         , id = 'data_table'
     )
 
@@ -32,13 +45,13 @@ app.layout = html.Div([
         
         html.Div([    
             html.Div(children=[
-                        generate_table(df),
+                        html.Div(id='div-table', children=generate_table(df)),
                         html.Div(id='row-selected', children='Selected row')],
-                    className='three columns'
+                    className='four columns'
                     ),
                 html.Div(children=[
                         html.H2(children='Chart goes here')],
-                    className='nine columns'
+                    className='eight columns'
                     ),
             ], className = 'row')
 
@@ -48,6 +61,14 @@ app.layout = html.Div([
 #This is a bad implementation, should be changed to a hidden div, see: https://community.plot.ly/t/app-not-resetting-with-page-refresh/5020/10
 #https://plot.ly/dash/sharing-data-between-callbacks
 CLICKS = OrderedDict([(df.iloc[i]['street'], 0) for i in range(len(df))])
+
+# updates table and chart - causes infinite loop currently
+#@app.callback(Output('div-table', 'children'),
+#              [Input('row-selected','children')] )
+#def update_table(street_name):
+#    table = generate_table(df, street_name)
+#    return table;
+
 
 @app.callback(Output('row-selected','children'),
               [Input(df.iloc[i]['street'], 'n_clicks') for i in range(len(df))] )
@@ -64,7 +85,7 @@ def button_click(*rows):
     return state_clicked
 
 app.css.append_css({
-    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+    'external_url': 'https://cityoftoronto.github.io/bdit_king_pilot_dashboard/css/dashboard.css'
 })
     
 if __name__ == '__main__':
