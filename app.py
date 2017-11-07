e46d73b9f2656b8cd44583db860c774fa73cc5a4
Raphael Dumas <raphael.a.dumas@gmail.com>
2017-11-07 16:50:33 -0500

    Dynamically generate callback function for each row
    callback detects whether row has been clicked and applies css classes
    accordingly to the table

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ from dash.dependencies import Input, Output, State
 
 import pandas as pd
 
-df = pd.read_csv("data/test.csv")
+DATA = pd.read_csv("data/test.csv")
+
+HIDDEN_DIV_ID = 'clicks_storage'
+TABLE_DIV_ID = 'div-table'
 
 def generate_cell_class(colNum):
     if colNum == 0:
@@ -16,14 +19,14 @@ def generate_cell_class(colNum):
     else:
         return 'segother'
     
-def generate_row_class(selected_str, current_str):
-    # if selected_str == current_str:
-    if current_str == 'Queen':
-        return 'selected';
+def generate_row_class(clicked):
+    '''Assigns class to clicked row'''
+    if clicked:
+        return 'selected'
     else:
-        return 'notselected';
+        return 'notselected'
 
-def generate_row(df_row, street_name = None):
+def generate_row(df_row, row_state):
     '''Create an HTML row from a database row
     '''
     return html.Tr([
@@ -32,29 +35,26 @@ def generate_row(df_row, street_name = None):
             html.Td(df_row.eb_base, className = generate_cell_class(2)),
             html.Td(df_row.wb_after, className = generate_cell_class(3)),
             html.Td(df_row.wb_base, className = generate_cell_class(4))
-        ], id= df_row.street, className = generate_row_class(street_name, df_row.street), n_clicks=0) 
+        ], id= df_row.street, className = generate_row_class(row_state['clicked']), n_clicks=row_state['n_clicks']) 
 
-def generate_table(dataframe):
-    return html.Table(
-           [html.Tr( [html.Td(""), html.Td("Eastbound",colSpan=2), html.Td("Westbound",colSpan=2)] )] +
-           [html.Tr( [html.Td(""), html.Td("After"), html.Td("Baseline"), html.Td("After"), html.Td("Baseline")] )] +
-           [generate_row(row) for row in dataframe.itertuples()]
-        , id='data_table')
+
 
 app = dash.Dash()
-CLICKS = OrderedDict([(df.iloc[i]['street'], dict(n_clicks=0, clicked=False)) for i in range(len(df))])
-
-
-
-#Super critical to store in an OrderedDict
-#This is a bad implementation, should be changed to a hidden div, see: https://community.plot.ly/t/app-not-resetting-with-page-refresh/5020/10
-#https://plot.ly/dash/sharing-data-between-callbacks
+app.config['suppress_callback_exceptions']=True
+CLICKS = OrderedDict([( row.street, dict(n_clicks=0, clicked=(row.street=='Queen'))) for row in DATA.itertuples()])
 
 def deserialise_clicks(clicks_json):
     return json.loads(clicks_json, object_pairs_hook=OrderedDict)
 
 def serialise_clicks(clicks_dict):
     return json.dumps(clicks_dict)
+
+def generate_table(state_data_dict):
+    return html.Table(
+           [html.Tr( [html.Td(""), html.Td("Eastbound",colSpan=2), html.Td("Westbound",colSpan=2)] )] +
+           [html.Tr( [html.Td(""), html.Td("After"), html.Td("Baseline"), html.Td("After"), html.Td("Baseline")] )] +
+           [generate_row(row, row_state) for row, row_state in zip(DATA.itertuples(), state_data_dict.values())]
+        , id='data_table')
 
 app.layout = html.Div([
 #        html.Div(children=[
@@ -63,21 +63,31 @@ app.layout = html.Div([
         
         html.Div([    
             html.Div(children=[
-                        html.Div(id='div-table', children=generate_table(df)),
-                        html.Div(id='row-selected', children='Selected row')],
+                        html.Div(id=TABLE_DIV_ID, children=generate_table(CLICKS))],
                     className='four columns'
                     ),
                 html.Div(children=[
-                        html.H2(children='Chart goes here')],
+                        html.H2(id='row-selected', children='Selected row')],
                     className='eight columns'
                     ),
             ], className = 'row'),
-        html.Div(id='clicks_storage', style={'display': 'none'}, children=serialise_clicks(CLICKS))
+        html.Div(id=HIDDEN_DIV_ID, style={'display': 'none'}, children=serialise_clicks(CLICKS))
         ])
 
-@app.callback(Output('clicks_storage','children'),
-              [Input(df.iloc[i]['street'], 'n_clicks') for i in range(len(df))],
-              [State('clicks_storage','children')] )
+def create_row_click_function(streetname):
+    @app.callback(Output(streetname, 'className'),
+                [Input(HIDDEN_DIV_ID, 'children')])
+    def update_clicked_row(state_data):
+        state_data_dict = deserialise_clicks(state_data)
+        return generate_row_class(state_data_dict[streetname]['clicked'])
+    update_clicked_row.__name__ = 'update_row_'+streetname
+    return update_clicked_row
+
+[create_row_click_function(key) for key in CLICKS.keys()]
+
+@app.callback(Output(HIDDEN_DIV_ID,'children'),
+              [Input(DATA.iloc[i]['street'], 'n_clicks') for i in range(len(DATA))],
+              [State(HIDDEN_DIV_ID,'children')] )
 def button_click(*args):
     rows, old_clicks = args[:-1], args[-1]
     clicks = deserialise_clicks(old_clicks)
@@ -90,7 +100,7 @@ def button_click(*args):
     return serialise_clicks(clicks)
 
 @app.callback(Output('row-selected', 'children'),
-              [Input('clicks_storage', 'children')])
+              [Input(HIDDEN_DIV_ID, 'children')])
 def update_row_clicked(click_state):
     clicks = deserialise_clicks(click_state)
     return [street_name for street_name, click_obj in clicks.items() if click_obj['clicked']]
