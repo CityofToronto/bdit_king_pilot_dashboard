5fa41b3c8405a272e8c96685cdf47a0611a4e982
ReedRodgers <reed.rodgers@toronto.ca>
2017-11-08 14:15:39 -0500

    Creates baselines table referencing baseline_dates.
    Closes Issue #57, builds on Issue #46. Uses inner join with baseline_dates to eliminate any dates we wanted excluded from the baseline, then aggregates the data into 30 minute buckets over one weekday and one weekend for each segment.

---
+++ bluetooth/create_baselines.sql
@@ -0,0 +1,15 @@
+﻿DROP TABLE baselines;
+create table baselines as (
+SELECT datetime_bin::time as time, 
+    avg(bt.travel_time) as avg_tt,
+    bt.analysis_id,
+    dates.daytype
+
+FROM dt_30min_agg bt
+    INNER JOIN baseline_dates dates ON (dates.analysis_id = bt.analysis_id AND dates.date = bt.datetime_bin::date) 
+
+GROUP BY bt.analysis_id, 
+    datetime_bin::time, 
+    dates.daytype);
+    
+ SELECT * FROM baselines
