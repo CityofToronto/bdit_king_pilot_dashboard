95b47308506839c1d3d52fecba6621e78ebc6684
Raphael Dumas <raphael.a.dumas@gmail.com>
2017-11-08 13:32:12 -0500

    Fix #54
    Separate into BASELINE and DATA global variables
    Create filter function to filter data by timeperiod

--- app.py
+++ app.py
@@ -9,8 +9,11 @@ from dash.dependencies import Input, Output, State
 
 import pandas as pd
 
-DATA = pd.read_csv("data/test.csv")
+DATA = pd.read_csv("data/bt_king_fake_data.csv")
+BASELINE = pd.read_csv("data/test.csv")
 
+STREETS = BASELINE['street']
+TIMEPERIODS = DATA['period'].unique()
 HIDDEN_DIV_ID = 'clicks_storage'
 TABLE_DIV_ID = 'div-table'
 
@@ -27,15 +30,15 @@ def generate_row_class(clicked):
     else:
         return 'notselected'
 
-def generate_row(df_row, row_state):
+def generate_row(df_row, baseline_row, row_state):
     '''Create an HTML row from a database row
     '''
     return html.Tr([
             html.Td(df_row.street, className = generate_cell_class(0)),
-            html.Td(df_row.eb_after, className = generate_cell_class(1)),
-            html.Td(df_row.eb_base, className = generate_cell_class(2)),
-            html.Td(df_row.wb_after, className = generate_cell_class(3)),
-            html.Td(df_row.wb_base, className = generate_cell_class(4))
+            html.Td(df_row.EB, className = generate_cell_class(1)),
+            html.Td(baseline_row.eb_base, className = generate_cell_class(2)),
+            html.Td(df_row.WB, className = generate_cell_class(3)),
+            html.Td(baseline_row.wb_base, className = generate_cell_class(4))
         ], id= df_row.street, className = generate_row_class(row_state['clicked']), n_clicks=row_state['n_clicks']) 
 
 
@@ -45,7 +48,7 @@ app = dash.Dash()
 server = app.server
 server.secret_key = os.environ.get('SECRET_KEY', 'my-secret-key')
 
-CLICKS = OrderedDict([( row.street, dict(n_clicks=0, clicked=(row.street=='Queen'))) for row in DATA.itertuples()])
+INITIAL_STATE = OrderedDict([(street, dict(n_clicks=0, clicked=(street =='Queen'))) for street in STREETS])
 
 def deserialise_clicks(clicks_json):
     return json.loads(clicks_json, object_pairs_hook=OrderedDict)
@@ -53,11 +56,22 @@ def deserialise_clicks(clicks_json):
 def serialise_clicks(clicks_dict):
     return json.dumps(clicks_dict)
 
-def generate_table(state_data_dict):
+def filter_data(timeperiod):
+    '''Return data aggregated and filtered by timeperiod
+    '''
+    filtered = DATA[DATA['period']==timeperiod].groupby(by=['street','direction'], as_index=False).mean()
+    pivoted = filtered.pivot_table(index='street', columns='direction', values='tt').reset_index()
+    pivoted.street = pivoted.street.astype("category")
+    pivoted.street.cat.set_categories(STREETS, inplace=True)
+    return pivoted.sort_values(['street'])
+
+
+def generate_table(state_data_dict, timeperiod = 'AMPK'):
+    filtered_data = filter_data(timeperiod)
     return html.Table(
            [html.Tr( [html.Td(""), html.Td("Eastbound",colSpan=2), html.Td("Westbound",colSpan=2)] )] +
            [html.Tr( [html.Td(""), html.Td("After"), html.Td("Baseline"), html.Td("After"), html.Td("Baseline")] )] +
-           [generate_row(row, row_state) for row, row_state in zip(DATA.itertuples(), state_data_dict.values())]
+           [generate_row(new_row, baseline_row, row_state) for new_row, baseline_row, row_state in zip(filtered_data.itertuples(), BASELINE.itertuples(), state_data_dict.values())]
         , id='data_table')
 
 app.layout = html.Div([
@@ -67,7 +81,7 @@ app.layout = html.Div([
         
         html.Div([    
             html.Div(children=[
-                        html.Div(id=TABLE_DIV_ID, children=generate_table(CLICKS))],
+                        html.Div(id=TABLE_DIV_ID, children=generate_table(INITIAL_STATE))],
                     className='four columns'
                     ),
                 html.Div(children=[
@@ -75,7 +89,7 @@ app.layout = html.Div([
                     className='eight columns'
                     ),
             ], className = 'row'),
-        html.Div(id=HIDDEN_DIV_ID, style={'display': 'none'}, children=serialise_clicks(CLICKS))
+        html.Div(id=HIDDEN_DIV_ID, style={'display': 'none'}, children=serialise_clicks(INITIAL_STATE))
         ])
 
 def create_row_click_function(streetname):
@@ -93,10 +107,10 @@ def create_row_click_function(streetname):
     update_clicked_row.__name__ = 'update_row_'+streetname
     return update_clicked_row
 
-[create_row_click_function(key) for key in CLICKS.keys()]
+[create_row_click_function(key) for key in INITIAL_STATE.keys()]
 
 @app.callback(Output(HIDDEN_DIV_ID,'children'),
-              [Input(DATA.iloc[i]['street'], 'n_clicks') for i in range(len(DATA))],
+              [Input(street, 'n_clicks') for street in STREETS],
               [State(HIDDEN_DIV_ID,'children')] )
 def button_click(*args):
     rows, old_clicks = args[:-1], args[-1]
