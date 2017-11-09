42eab302e5e2c23aeab17f43699945089cef6498
Aakash Harpalani <aharpal@toronto.ca>
2017-11-09 10:59:59 -0500

    Script and data for `bt_segments`

---
+++ bluetooth/sql/create-table-bt_segments.sql
@@ -0,0 +1,9 @@
+﻿DROP TABLE IF EXISTS king_pilot.bt_segments;
+
+CREATE TABLE king_pilot.bt_segments (
+	bt_id int NOT NULL,
+	analysis_id bigint NOT NULL,
+	street_name text,
+	direction text,
+	from_intersection text,
+	to_intersection text);
