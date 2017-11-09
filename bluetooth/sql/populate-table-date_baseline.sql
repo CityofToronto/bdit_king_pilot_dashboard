4e7ee39a9ee62bf20a4102b65c1d796d33aaa2de
Aakash Harpalani <aharpal@toronto.ca>
2017-11-09 14:10:16 -0500

    Scripts for `date_baseline`

---
+++ bluetooth/sql/populate-table-date_baseline.sql
@@ -0,0 +1,13 @@
+﻿INSERT INTO king_pilot.date_baseline
+SELECT B.bt_id, A.daytype as day_type, A.date as dt
+FROM rrodger.baseline_dates A
+INNER JOIN king_pilot.bt_segments B USING (analysis_id)
+ORDER BY B.bt_id, A.date;
+
+UPDATE king_pilot.date_baseline
+SET day_type = 'Weekday'
+WHERE day_type = 'weekday';
+
+UPDATE king_pilot.date_baseline
+SET day_type = 'Weekend'
+WHERE day_type = 'weekend';
