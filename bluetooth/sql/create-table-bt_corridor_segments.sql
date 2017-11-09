2170779bbd8619551cfcc94b805542b8ffe4f9ba
Aakash Harpalani <aharpal@toronto.ca>
2017-11-09 11:56:43 -0500

    script and data for `bt_corridor_segments`

---
+++ bluetooth/sql/create-table-bt_corridor_segments.sql
@@ -0,0 +1,9 @@
+﻿DROP TABLE IF EXISTS king_pilot.bt_corridor_segments;
+
+CREATE TABLE king_pilot.bt_corridor_segments (
+	corridor_segment_id int NOT NULL,
+	bt_id int,
+	order_id int);
+	
+GRANT ALL ON TABLE king_pilot.bt_corridor_segments TO aharpal;
+GRANT SELECT ON TABLE king_pilot.bt_corridor_segments TO public;
