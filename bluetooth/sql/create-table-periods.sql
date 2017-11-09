4cf6aa261cf5f6a4540ebb015ee82a7702ac3e24
Aakash Harpalani <aharpal@toronto.ca>
2017-11-09 12:18:07 -0500

    Script and data for `periods`

---
+++ bluetooth/sql/create-table-periods.sql
@@ -0,0 +1,10 @@
+﻿DROP TABLE IF EXISTS king_pilot.periods;
+
+CREATE TABLE king_pilot.periods (
+	period_id int NOT NULL,
+	day_type text,
+	period_name text,
+	period_range timerange);
+	
+GRANT ALL ON TABLE king_pilot.periods TO aharpal;
+GRANT SELECT ON TABLE king_pilot.periods TO public;
