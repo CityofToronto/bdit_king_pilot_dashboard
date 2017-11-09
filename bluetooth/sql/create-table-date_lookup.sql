368703d8e355ff54151c0879b5ca52cc8b63a38a
Aakash Harpalani <aharpal@toronto.ca>
2017-11-09 12:35:25 -0500

    Script and data for `date_lookup`

---
+++ bluetooth/sql/create-table-date_lookup.sql
@@ -0,0 +1,8 @@
+﻿DROP TABLE IF EXISTS king_pilot.date_lookup;
+
+CREATE TABLE king_pilot.date_lookup (
+	dt date,
+	day_type text);
+	
+GRANT ALL ON TABLE king_pilot.periods TO aharpal;
+GRANT SELECT ON TABLE king_pilot.periods TO public;
