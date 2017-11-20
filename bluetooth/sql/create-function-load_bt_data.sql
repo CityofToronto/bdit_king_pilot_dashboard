ff9bb2fc9434def23507f10e3921aea89b9e78d5
Raphael Dumas <raphael.a.dumas@gmail.com>
2017-11-20 10:02:46 -0500

    Split king load_data function into two
    One King specific, the other not.
    https://github.com/CityofToronto/bdit_data-sources/issues/61

--- bluetooth/sql/create-function-load_bt_data.sql
+++ bluetooth/sql/create-function-load_bt_data.sql
@@ -1,32 +1,6 @@
 ﻿CREATE OR REPLACE FUNCTION king_pilot.load_bt_data() RETURNS integer AS $$
 
 BEGIN
-	-- TRANSFER bluetooth.raw_data TO bluetooth.observations
-	INSERT INTO bluetooth.observations (user_id,analysis_id,
-	  measured_time,
-	  measured_time_no_filter,
-	  startpoint_number,
-	  startpoint_name,
-	  endpoint_number,
-	  endpoint_name,
-	  measured_timestamp,
-	  outlier_level,
-	  cod,
-	  device_class)
-	SELECT * FROM bluetooth.raw_data rs;
-
-	-- LOAD bluetooth.aggr_5min with new data
-	INSERT INTO bluetooth.aggr_5min (analysis_id, datetime_bin, tt, obs)
-	SELECT	rs.analysis_id,
-		TIMESTAMP WITHOUT TIME ZONE 'epoch' +
-		INTERVAL '1 second' * (floor((extract('epoch' from rs.measured_timestamp)-1) / 300) * 300) as datetime_bin,
-		median(rs.measured_time) AS travel_time,
-		COUNT(rs.user_id) AS obs
-	FROM bluetooth.raw_data rs
-	WHERE rs.outlier_level = 0 AND device_class = 1
-	GROUP BY rs.analysis_id, (floor((extract('epoch' from rs.measured_timestamp)-1) / 300) * 300)
-	ORDER BY rs.analysis_id, (floor((extract('epoch' from rs.measured_timestamp)-1) / 300) * 300);
-
 	-- TRUNCATE king_pilot.tt_30min
 	TRUNCATE king_pilot.tt_30min;
 
@@ -41,9 +15,12 @@ BEGIN
 	FROM king_pilot.bt_segments A
 	INNER JOIN bluetooth.aggr_5min B USING (analysis_id)
 	WHERE NOT (A.bt_id = 33 AND B.datetime_bin::date = '2017-09-19' AND B.datetime_bin::time >= '19:00')
-	GROUP BY A.bt_id, B.datetime_bin::date, (TIMESTAMP WITHOUT TIME ZONE 'epoch' + INTERVAL '1 second' * (floor((extract('epoch' from B.datetime_bin)) / 1800) * 1800))::time
-	ORDER BY A.bt_id, B.datetime_bin::date,  (TIMESTAMP WITHOUT TIME ZONE 'epoch' + INTERVAL '1 second' * (floor((extract('epoch' from B.datetime_bin)) / 1800) * 1800))::time;
+	GROUP BY A.bt_id, dt, time_bin;
 
 	RETURN 1;
 END
 $$ LANGUAGE plpgsql;
+GRANT EXECUTE ON FUNCTION king_pilot.load_bt_data() TO bt_insert_bot;
+GRANT EXECUTE ON FUNCTION king_pilot.load_bt_data() TO rdumas;
+ ALTER FUNCTION king_pilot.load_bt_data()
+   OWNER TO bt_admins;
