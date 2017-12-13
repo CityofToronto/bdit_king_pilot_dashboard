5716130b64f7b7aa031e3fab17c60dd84fdadeb5
q-schen <31662227+q-schen@users.noreply.github.com>
2017-12-13 13:53:59 -0500

    SQL queries for avgtt baseline calculations

---
+++ ttc/sql/avgtt_Bathurst-to-Jarvis.sql
@@ -0,0 +1,81 @@
+﻿-- EB KING
+select period_name, round(avg(bl)/60,1) bl_min, round(avg(bl),1) bl_sec
+from 
+(
+	-- avg tt per time_bin for corridor
+	select period_name, time_bin, sum(sum(avgtt)) over (partition by time_bin) as bl
+	from
+	(
+		-- avg tt per time_bin per bt_id
+		select bt_id, period_name, time_bin, avg(tt) as avgtt
+		from king_pilot.ttc_tt_30min t
+		inner join king_pilot.periods on (time_bin::time <@ period_range
+							and day_type = (case 
+								when extract(dow from dt) = 0 then 'Weekend'
+								when extract(dow from dt) = 1 then 'Weekday'
+								when extract(dow from dt) = 2 then 'Weekday'
+								when extract(dow from dt) = 3 then 'Weekday'
+								when extract(dow from dt) = 4 then 'Weekday'
+								when extract(dow from dt) = 5 then 'Weekday'
+								when extract(dow from dt) = 6 then 'Weekend' end))
+		where bt_id >= 52 and bt_id <= 55 -- EB KING Bathurst to Jarvis
+		and dt >= '2017-09-24' and dt <= '2017-10-14'
+		and period_id in (1, 3) -- weekday AM Peak and PM Peak
+		group by bt_id, period_name, time_bin
+		order by bt_id, time_bin
+	)
+	as bt_avgtt
+	group by period_name, time_bin
+	order by time_bin
+)
+as corridor_avgtt
+group by period_name
+order by period_name
+
+
+
+
+
+
+
+
+-- WB KING
+select period_name, round(avg(bl)/60,1) bl_min, round(avg(bl),1) bl_sec
+from 
+(
+	-- avg tt per time_bin for corridor
+	select period_name, time_bin, sum(sum(avgtt)) over (partition by time_bin) as bl
+	from
+	(
+		-- avg tt per time_bin per bt_id
+		select bt_id, period_name, time_bin, avg(tt) as avgtt
+		from king_pilot.ttc_tt_30min t
+		inner join king_pilot.periods on (time_bin::time <@ period_range
+							and day_type = (case 
+								when extract(dow from dt) = 0 then 'Weekend'
+								when extract(dow from dt) = 1 then 'Weekday'
+								when extract(dow from dt) = 2 then 'Weekday'
+								when extract(dow from dt) = 3 then 'Weekday'
+								when extract(dow from dt) = 4 then 'Weekday'
+								when extract(dow from dt) = 5 then 'Weekday'
+								when extract(dow from dt) = 6 then 'Weekend' end))
+		where bt_id >= 60 and bt_id <= 63 -- WB KING Jarvis to Bathurst
+		and dt >= '2017-09-24' and dt <= '2017-10-14'
+		and period_id in (1, 3) -- weekday AM Peak and PM Peak
+		group by bt_id, period_name, time_bin
+		order by bt_id, time_bin
+	)
+	as bt_avgtt
+	group by period_name, time_bin
+	order by time_bin
+)
+as corridor_avgtt
+group by period_name
+order by period_name
+
+
+
+
+
+
+
