547316ca97c78c780ccf87a2245ce80e97d8e500
Aakash Harpalani <aharpal@toronto.ca>
2017-12-14 15:46:12 -0500

    more miovision processing SQL scripts

---
+++ miovision/sql/create-table-movements.sql
@@ -0,0 +1,20 @@
+﻿DROP TABLE IF EXISTS miovision.movements;
+
+CREATE TABLE miovision.movements (
+	movement_uid serial,
+	movement text,
+	location_only boolean);
+
+ALTER TABLE miovision.movements
+  OWNER TO aharpal;
+GRANT ALL ON TABLE miovision.movements TO rds_superuser WITH GRANT OPTION;
+GRANT ALL ON TABLE miovision.movements TO dbadmin;
+GRANT SELECT, REFERENCES, TRIGGER ON TABLE miovision.movements TO bdit_humans WITH GRANT OPTION;
+GRANT ALL ON TABLE miovision.movements TO aharpal;
+
+INSERT INTO miovision.movements(movement, location_only) VALUES ('Thru', FALSE);
+INSERT INTO miovision.movements(movement, location_only) VALUES ('Left', FALSE);
+INSERT INTO miovision.movements(movement, location_only) VALUES ('Right', FALSE);
+INSERT INTO miovision.movements(movement, location_only) VALUES ('U-Turn', FALSE);
+INSERT INTO miovision.movements(movement, location_only) VALUES ('Peds CW', TRUE);
+INSERT INTO miovision.movements(movement, location_only) VALUES ('Peds CCW', TRUE);
---
+++ miovision/sql/populate-table-volumes.sql
@@ -0,0 +1,10 @@
+﻿TRUNCATE miovision.volumes;
+
+INSERT INTO miovision.volumes (intersection_uid, datetime_bin, classification_uid, leg, movement_uid, volume)
+SELECT B.intersection_uid, A.datetime_bin, C.classification_uid, A.entry_name as leg, D.movement_uid, A.volume
+FROM miovision.raw_data A
+INNER JOIN miovision.intersections B ON TRIM(substring(replace(A.study_name,'Adelaide/','Adelaide /') FROM '(?:(?!Oct|Nov).)*')) = B.intersection_name
+INNER JOIN miovision.movements D USING (movement)
+INNER JOIN miovision.classifications C USING (classification, location_only)
+WHERE A.lat = B.lat AND A.lng = B.lng
+ORDER BY A.datetime_bin, B.intersection_uid, C.classification_uid, A.entry_name, D.movement_uid;
