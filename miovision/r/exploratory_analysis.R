d01b3e7d9385a80434ca54dd45d9b3c6150a97cb
Aakash Harpalani <aharpal@toronto.ca>
2018-01-08 15:40:31 -0500

    Exploratory Analysis - TOD plots

--- miovision/r/exploratory_analysis.R
+++ miovision/r/exploratory_analysis.R
@@ -1,37 +1,14 @@
-Exploratory Analysis - Miovision Counts
-================
-Aakash Harpalani
-Thursday, December 14, 2017
-
-### Setup
-
-Loading libraries:
-
-``` r
 library(RPostgreSQL)
 library(ggplot2)
 library(lubridate)
 library(dplyr)
-```
+library(ggthemes)
 
-Establishing connections and credentials:
-
-``` r
 drv <- dbDriver("PostgreSQL")
 source("connect/connect.R")
-```
 
-### Analysis
-
-Set intersection of interest:
-
-``` r
 intersection_id = 1
-```
 
-Retrieve 15-min count data:
-
-``` r
 strSQL <-
   paste0("SELECT intersection_name FROM miovision.intersections WHERE intersection_uid = ",intersection_id)
 intersection_name <- dbGetQuery(con, strSQL)
@@ -43,40 +20,19 @@ strSQL <-
          "WHERE intersection_uid = ",intersection_id," ",
          "ORDER BY datetime_bin, classification_uid, leg, dir ")
 data <- dbGetQuery(con, strSQL)
-```
 
-Overall volumes, by date/tod
 
-``` r
 all_vehicles <- data %>%
   group_by(datetime_bin, leg, dir) %>%
   filter(classification %in% c('Lights','Single-Unit Trucks','Articulated Trucks','Buses')) %>%
   summarize(total_volume = sum(volume))
 head(all_vehicles)
-```
 
-    ## Source: local data frame [6 x 4]
-    ## Groups: datetime_bin, leg [4]
-    ## 
-    ##          datetime_bin   leg   dir total_volume
-    ##                <dttm> <chr> <chr>        <int>
-    ## 1 2017-10-11 11:00:00     E    EB          127
-    ## 2 2017-10-11 11:00:00     N    NB          131
-    ## 3 2017-10-11 11:00:00     N    SB          136
-    ## 4 2017-10-11 11:00:00     S    NB          211
-    ## 5 2017-10-11 11:00:00     S    SB           89
-    ## 6 2017-10-11 11:15:00     E    EB          147
-
-``` r
 all_vehicles$dt <- floor_date(all_vehicles$datetime_bin,'1 day')
 all_vehicles$time_bin <- as.POSIXct(paste(sprintf("%02d",hour(all_vehicles$datetime_bin)),sprintf("%02d",minute(all_vehicles$datetime_bin)),sep=":"), format = "%H:%M")
 all_vehicles$wk <- strftime(all_vehicles$dt,format='%W')
 all_vehicles$dow <- strftime(all_vehicles$dt, format = '%u')
-```
 
-Plot all direction/leg combinations:
-
-``` r
 for (leg in c('N','S','W','E')){
   for (dir in c('NB','SB','EB','WB')){
     if (((leg %in% c('N','S')) && (dir %in% c('NB','SB'))) | ((leg %in% c('E','W')) && (dir %in% c('EB','WB')))){
@@ -87,7 +43,7 @@ for (leg in c('N','S','W','E')){
       dates$x = as.POSIXct('06:00', format = '%H:%M')
       dates$y = 0.975 * max(subset$total_volume)
       
-      print(ggplot(data = subset, aes(x=time_bin, y=total_volume)) +
+      ggplot(data = subset, aes(x=time_bin, y=total_volume)) +
         geom_line(aes(color = ifelse(dow < 6, "weekday", "weekend")), size = 1) +
         scale_colour_brewer(type = 'qua', palette = "Set1", direction = -1) +
         scale_x_datetime(date_labels = "%H", date_breaks = "2 hours") +
@@ -95,25 +51,7 @@ for (leg in c('N','S','W','E')){
         geom_text(data = dates, aes(x,y-0.5,label=label)) +
         theme_light() + 
         guides(color = F) +
-        labs(title = paste0(intersection_name,": ", leg, " leg, ", dir, " direction")))
+        labs(title = paste0(intersection_name,": ", leg, " leg, ", dir, " direction"))
     }
   }
 }
-```
-
-![](exploratory_analysis_files/figure-markdown_github/unnamed-chunk-6-1.png)![](exploratory_analysis_files/figure-markdown_github/unnamed-chunk-6-2.png)![](exploratory_analysis_files/figure-markdown_github/unnamed-chunk-6-3.png)![](exploratory_analysis_files/figure-markdown_github/unnamed-chunk-6-4.png)
-
-    ## geom_path: Each group consists of only one observation. Do you need to
-    ## adjust the group aesthetic?
-
-![](exploratory_analysis_files/figure-markdown_github/unnamed-chunk-6-5.png)
-
-    ## geom_path: Each group consists of only one observation. Do you need to
-    ## adjust the group aesthetic?
-
-![](exploratory_analysis_files/figure-markdown_github/unnamed-chunk-6-6.png)![](exploratory_analysis_files/figure-markdown_github/unnamed-chunk-6-7.png)
-
-    ## geom_path: Each group consists of only one observation. Do you need to
-    ## adjust the group aesthetic?
-
-![](exploratory_analysis_files/figure-markdown_github/unnamed-chunk-6-8.png)
